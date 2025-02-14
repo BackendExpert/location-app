b8311d90011440f7bbfaeeed55f1aa7f9f7ed494
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-02-14 07:35:28 +0530

    Update

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,7 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet, ImageBackground } from "react-native";
 
 export default function Index() {
+    const bgImg = require("../assets/images/UOPWelcome.jpg");
   return (
     <View
       style={{
@@ -9,7 +10,43 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <ImageBackground source={bgImg} style={styles.background} resizeMode="cover">
+        <View style={styles.overlay}>
+          <View style={styles.headerWelcome}>
+          <Text style={styles.headerText}>Welcome to</Text>
+          </View>
+
+        </View>
+
+      </ImageBackground>
+
+
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  background: {
+    width: "100%",
+    height: "100%",
+  },
+  overlay: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0, 
+    backgroundColor: "rgba(0, 0, 0, 0.6)",
+  },
+  headerWelcome: {
+    marginTop: 30
+  },
+  headerText: {
+    textAlign: 'center',
+    color: 'white',
+    fontSize: 20
+  }
+})
